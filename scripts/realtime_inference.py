f63d00a90d22ef53b758d76603c0008aca4881de
andrewhsugithub <andrew1mail@gmail.com>
2024-08-27 02:31:28 +0800

    fix: ffmpeg whitespace

--- scripts/realtime_inference.py
+++ scripts/realtime_inference.py
@@ -260,7 +260,7 @@ class Avatar:
         # Close the queue and sub-thread after all tasks are completed
         process_thread.join()
         
-        if args.skip_save_images is True:
+        if skip_save_images is True:
             print('Total process time of {} frames without saving images = {}s'.format(
                         video_num,
                         time.time()-start_time))
@@ -269,14 +269,14 @@ class Avatar:
                         video_num,
                         time.time()-start_time))
 
-        if out_vid_name is not None and args.skip_save_images is False: 
+        if out_vid_name is not None and skip_save_images is False: 
             # optional
-            cmd_img2video = f"ffmpeg -y -v warning -r {fps} -f image2 -i {self.avatar_path}/tmp/%08d.png -vcodec libx264 -vf format=rgb24,scale=out_color_matrix=bt709,format=yuv420p -crf 18 {self.avatar_path}/temp.mp4"
+            cmd_img2video = f"ffmpeg -y -v warning -r {fps} -f image2 -i \'{self.avatar_path}/tmp/%08d.png\' -vcodec libx264 -vf format=rgb24,scale=out_color_matrix=bt709,format=yuv420p -crf 18 \'{self.avatar_path}/temp.mp4\'"
             print(cmd_img2video)
             os.system(cmd_img2video)
 
             output_vid = os.path.join(self.video_out_path, out_vid_name+".mp4") # on
-            cmd_combine_audio = f"ffmpeg -y -v warning -i {audio_path} -i {self.avatar_path}/temp.mp4 {output_vid}"
+            cmd_combine_audio = f"ffmpeg -y -v warning -i \'{audio_path}\' -i \'{self.avatar_path}/temp.mp4\' \'{output_vid}\'"
             print(cmd_combine_audio)
             os.system(cmd_combine_audio)
 
